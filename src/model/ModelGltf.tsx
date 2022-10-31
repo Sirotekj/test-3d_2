5d5fbcb975a47dab2b696217930b8a00f4ed59ba
Jakub Sirotek <sirotekj@rferl.org>
2022-10-31 14:05:29 +0100

    First push

---
+++ src/model/ModelGltf.tsx
@@ -0,0 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Suspense } from "react";
+
+
+function ModelGltf(){
+  /*const gltf = useLoader(GLTFLoader, './BayraktarEmbedded.gltf');
+  console.log(gltf);
+  return (
+    <Canvas>
+      <Suspense fallback={null}>
+        <primitive object={gltf.scene} scale={0.4} />
+      </Suspense>
+    </Canvas>
+  )*/
+  return <></>
+}
+
+export default ModelGltf;
---
+++ src/model/ModelObj.tsx
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from '@react-three/fiber'
+import { Environment, OrbitControls } from "@react-three/drei";
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { Suspense } from "react";
+
+
+const Scene = () => {
+  //const materials = useLoader(MTLLoader, "model/Bayraktar.mtl");
+  //const obj = useLoader(OBJLoader, "model/Bayraktar.obj", (loader) => {
+  const materials = useLoader(MTLLoader, "https://docs.rferl.org/Infographics/labs/Jakub/Test3D/model/Bayraktar.mtl");
+  const obj = useLoader(OBJLoader, "https://docs.rferl.org/Infographics/labs/Jakub/Test3D/model/Bayraktar.obj", (loader) => {
+    materials.preload();
+    loader.setMaterials(materials);
+  });
+
+  console.log(obj);
+  return <primitive object={obj} scale={4} />;
+};
+
+function ModelObj(){
+  return (
+    <Canvas>
+      <Suspense fallback={null}>
+        <Scene />
+        <OrbitControls />
+        <Environment preset="sunset" background />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default ModelObj;
